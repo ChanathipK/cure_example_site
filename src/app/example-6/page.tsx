ebda4b699a12aec8399c266a774e9902d5eed905
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2025-01-21 13:07:53 +0700

    fix misconception

--- src/app/example-6/page.tsx
+++ src/app/example-6/page.tsx
@@ -20,7 +20,7 @@ export default function ExampleSix() {
                 <div className="h-96 bg-blue-300 p-4">
                     <div className="flex gap-2">
                         <button className="px-4 py-2 bg-white rounded-md hover:bg-slate-100 active:bg-slate-200" onClick={testScreenSize}>Test Screen Size</button>
-                        <p className="px-4 py-2 bg-white rounded-md">This button shows screen size. Screen size is the resolution of the screen. It's not affected by zoom but affected by scaling.</p>
+                        <p className="px-4 py-2 bg-white rounded-md">This button shows screen size. Screen size is the resolution of the screen. It's affected by zoom and scaling.</p>
                     </div>
                     <div className="flex gap-2 mt-2">
                         <button className="px-4 py-2 bg-white rounded-md hover:bg-slate-100 active:bg-slate-200" onClick={testViewportSize}>Test Viewport Size</button>
